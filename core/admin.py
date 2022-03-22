c4ea6c9fcfca964f70c9a877c8463e2be9e376ed
luizfelipevbll <luizfelipevbll@gmail.com>
2022-03-22 02:56:29 -0300

    feat: impl. Custom User, Managers and static/media stuff

---
+++ core/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from core import models
+
+admin.site.site_header = "CRUD Administration"
+admin.site.site_title = "CRUD Portal Admin"
+admin.site.index_title = "Welcome to CRUD Portal"
+
+
+@admin.register(models.User)
+class RoutineAdmin(admin.ModelAdmin):
+    filter_horizontal = ["groups", "user_permissions"]
+    list_display = [
+        "email",
+        "first_name",
+        "birth",
+        "is_staff",
+        "is_superuser",
+        "is_active",
+    ]
+    search_fields = ["email", "first_name", "last_name"]
+    list_filter = ["is_active", "is_active", "is_deleted"]
+    per_page = 20
---
+++ core/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from core import managers
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, blank=False, null=False)
+    first_name = models.CharField(max_length=100, blank=False, null=False)
+    last_name = models.CharField(max_length=100, blank=False, null=False)
+    birth = models.DateField("Birth Date", blank=False, null=True)
+    is_active = models.BooleanField("Active", default=True, blank=False, null=False)
+    is_staff = models.BooleanField("Staff", default=False, blank=False, null=False)
+    is_superuser = models.BooleanField(
+        "Superuser", default=False, blank=False, null=False
+    )
+    is_deleted = models.BooleanField("Deleted", default=False, blank=False, null=False)
+    updated_at = models.DateTimeField(auto_now=True)
+    username = None
+    objects = managers.UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "User"
+
+    def get_username(self):
+        return self.email
+
+    def __str__(self) -> str:
+        return self.email
